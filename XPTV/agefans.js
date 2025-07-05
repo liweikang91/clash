80e64079fab6bada6554d2e602a0b0f31cb528f7
wiko <66823212+liweikang91@users.noreply.github.com>
2025-07-06 03:02:59 +0800

    Update agefans.js

--- XPTV/agefans.js
+++ XPTV/agefans.js
@@ -1,15 +1,15 @@
 const cheerio = createCheerio()
 const UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36"
 const headers = {
-  'Referer': 'https://m.agedm.live/',
-  'Origin': 'https://m.agedm.live',
+  'Referer': 'https://m.agedm.vip/',
+  'Origin': 'https://m.agedm.vip',
   'User-Agent': UA,
 }
 
 const appConfig = {
   ver: 1,
   title: "AGE",
-  site: "https://m.agedm.live/#/",
+  site: "https://m.agedm.vip/#/",
   tabs: [{
     name: '最近更新',
     ext: {
@@ -28,7 +28,7 @@ async function getCards(ext) {
   let id = ext.id
   let page = ext.page || 1
 
-  const url = `https://api.agedm.live/v2/catalog?genre=all&label=all&letter=all&order=time&region=all&resource=all&season=all&status=all&year=all&size=20&page=${page}`
+  const url = `https://api.agedm.vip/v2/catalog?genre=all&label=all&letter=all&order=time&region=all&resource=all&season=all&status=all&year=all&size=20&page=${page}`
 
   const { data } = await $fetch.get(url, {
     headers
@@ -41,7 +41,7 @@ async function getCards(ext) {
       vod_pic: each.cover,
       vod_remarks: each.uptodate,
       ext: {
-        url: `https://api.agedm.live/v2/detail/${each.id}`,
+        url: `https://api.agedm.vip/v2/detail/${each.id}`,
       },
     });
   })
@@ -117,7 +117,7 @@ async function search(ext) {
   let text = encodeURIComponent(ext.text)
   let page = ext.page || 1
 
-  const url = `https://api.agedm.live/v2/search?query=${text}&page=${page}`
+  const url = `https://api.agedm.vip/v2/search?query=${text}&page=${page}`
   const { data } = await $fetch.get(url, {
     headers
   })
@@ -129,7 +129,7 @@ async function search(ext) {
       vod_pic: each.cover,
       vod_remarks: each.uptodate,
       ext: {
-        url: `https://api.agedm.live/v2/detail/${each.id}`,
+        url: `https://api.agedm.vip/v2/detail/${each.id}`,
       },
     });
   })
